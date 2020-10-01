8c6508bddf55ce7ad8dfa2dfca997f691c050210
pamu47 <pamudithamaduranga@gmail.com>
2020-10-01 23:31:40 +0530

    api to get auth uri

--- client/src/App.js
+++ client/src/App.js
@@ -1,25 +1,33 @@
 import React from 'react';
-import logo from './logo.svg';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Homepage from './components/Homepage'
+import Auth from './components/Auth'
+
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <Router>
+    <div className="container" style={{ marginTop: '20px' }}>
+      <nav class="navbar navbar-expand-lg navbar-light bg-light">
+        <a class="navbar-brand" href="/abc">Navbar</a>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+          <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+          <div class="navbar-nav">
+            <a class="nav-item nav-link active" href="/abc">Home <span class="sr-only">(current)</span></a>
+            <a class="nav-item nav-link" href="/abc">Features</a>
+            <a class="nav-item nav-link" href="/abc">Pricing</a>
+            <a class="nav-item nav-link disabled" href="/abc" tabindex="-1" aria-disabled="true">Disabled</a>
+          </div>
+        </div>
+      </nav>
     </div>
+    <Route path="/" exact component={Homepage}/>
+    <Route path="/auth" component={Auth}/>
+  </Router>
   );
 }
 
