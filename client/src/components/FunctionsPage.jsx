727427868e93acc5ac3d3292c36da7eb4e0a9ff9
pamu47 <pamudithamaduranga@gmail.com>
2020-10-03 18:32:57 +0530

    UIs updated

--- client/src/components/FunctionsPage.jsx
+++ client/src/components/FunctionsPage.jsx
@@ -1,5 +1,7 @@
 import React, { Component } from "react";
 import Axios from "axios";
+import swal from "sweetalert2";
+import './stylesheets/functions.css'
 
 class FunctionsPage extends Component {
   constructor() {
@@ -7,7 +9,12 @@ class FunctionsPage extends Component {
     this.state = {
       name: "",
       picture: "",
+      showForm: false,
+      file: "",
     };
+    this.displayForm = this.displayForm.bind(this);
+    this.onChange = this.onChange.bind(this);
+    this.onSubmit = this.onSubmit.bind(this);
   }
 
   componentDidMount() {
@@ -20,13 +27,59 @@ class FunctionsPage extends Component {
     });
   }
 
+  displayForm() {
+    this.setState({
+      showForm: true,
+    });
+  }
+
+  onChange(e) {
+    // console.log(e.target.files[0]);
+    this.setState({
+      file: e.target.files[0],
+    });
+    console.log(this.state.file);
+  }
+
+  onSubmit(e) {
+    e.preventDefault();
+    const data = new FormData();
+    data.append("file", this.state.file);
+    Axios.post("http://localhost:5000/google/upload", data).then((res) => {
+      if (res.data.status === 500) {
+        console.log(res.data);        
+        swal.fire({
+          icon: "error",
+          title: "Oops...",
+          text: res.data.msg
+        });
+      } else if (res.data.status === 200) {
+        console.log(res.data);
+        swal.fire({
+          title: "Success",
+          text: res.data.msg,
+          icon: "success",
+        });
+      } else {
+        swal.fire({
+          icon: "error",
+          title: "Oops...",
+          text: res.data.msg
+        });
+      }
+    });
+  }
+
   render() {
     return (
-      <div className="container text-center">
+      <div
+        className="container text-center"
+        style={{ fontFamily: "Monospace" }}
+      >
         <div className="row" style={{ paddingTop: 50 }}>
-          <div className="col-6">
+          <div className="col-lg-6 col-md-6 col-sm-12">
             <div className="container text-left">
-              <h2 style={{ fontFamily: "Ubuntu" }}>{this.state.name}</h2>
+              <h2 style={{ fontFamily: "Monospace" }}>{this.state.name}</h2>
               <br />
               <img
                 src={this.state.picture}
@@ -35,11 +88,55 @@ class FunctionsPage extends Component {
               />
             </div>
           </div>
-          <div className="col-6" style={{ paddingTop: 50 }}>
-          <a href="/" class="btn btn-block btn-lg btn-success"><span class="fa fa-cloud-upload"></span> Upload Files</a>
-          <br/><hr/><br/>
-          <a href="/" class="btn btn-block btn-lg btn-info"><span class="fa fa-folder-open"></span> View Files</a>
+          <div className="col-lg-6 col-md-6 col-sm-12" style={{ paddingTop: 50 }}>
+            <button
+              onClick={this.displayForm}
+              class="btn btn-block btn-lg btn-success"
+            >
+              <span class="fa fa-cloud-upload"></span> Upload Files
+            </button>
+            <br />
+            <hr />
+            <br />
+            {this.state.showForm ? (
+              <div className="container card" style={{ padding:50 }}>
+                <h2 className="heading text-left text-uppercase">Upload</h2>
+                <br />
+                <form
+                  noValidate
+                  onSubmit={this.onSubmit}
+                  enctype="multipart/form-data"
+                >
+                  <div class="form-group text-left">
+                    <label htmlFor="exampleFile" className="font-weight-bold">
+                      Select a file to upload :
+                    </label>
+                    <br />
+                    <br />
+                    <input
+                      type="file"
+                      id="exampleFile"
+                      name="file"
+                      onChange={this.onChange}
+                    />
+                  </div>
+                  <input
+                    className="btn btn-success"
+                    type="submit"
+                    value="Upload"
+                  />
+                </form>
+              </div>
+            ) : (
+              <div></div>
+            )}
           </div>
+        </div>
+
+        {/* Form container */}
+        <div className="row">
+          <div className="col-6"></div>
+          <div className="col-6"></div>
         </div>
       </div>
     );
--- client/src/components/Homepage.jsx
+++ client/src/components/Homepage.jsx
@@ -1,44 +1,74 @@
-import React, {Component} from 'react';
-import Axios from 'axios';
-import { Link } from 'react-router-dom';
+import React, { Component } from "react";
+import Axios from "axios";
+import { Link } from "react-router-dom";
 
-class Homepage extends Component{
-    constructor(props){
-        super()
-        this.state = {
-            redirect_uri: ''
-        }
-        this.revokeToken = this.revokeToken.bind(this)
-    }
+class Homepage extends Component {
+  constructor(props) {
+    super();
+    this.state = {
+      redirect_uri: "",
+    };
+    this.revokeToken = this.revokeToken.bind(this);
+  }
 
-    componentDidMount(){
-        Axios.get('http://localhost:5000/').then(res => {
-            this.setState({
-                redirect_uri: res.data.url
-            })
-        })
-        console.log(this.state.redirect_uri)
-    }
+  componentDidMount() {
+    Axios.get("http://localhost:5000/").then((res) => {
+      this.setState({
+        redirect_uri: res.data.url,
+      });
+    });
+    console.log(this.state.redirect_uri);
+  }
 
-    revokeToken(){
-        Axios.get('http://localhost:5000/google/deleteToken').then(res => {
-            console.log("Done")
-        })
-    }
+  revokeToken() {
+    Axios.get("http://localhost:5000/google/deleteToken").then((res) => {
+      console.log("Done");
+    });
+  }
 
-    render(){
-        return(
-            <div className="container">
-                <h2>Homepage!</h2>
-                <br/>
-                <a href={this.state.redirect_uri} className="btn btn-block btn-primary"><span className="fa fa-google"></span> Login</a>
+  render() {
+    return (
+      <div className="container">
+        <h2 style={{ paddingTop: 40 }}>oAuth 2.0</h2>
+        
+        <p className="text-justify">
+          OAuth is an open standard for access delegation, commonly used as a
+          way for Internet users to grant websites or applications access to
+          their information on other websites but without giving them the
+          passwords.[1] This mechanism is used by companies such as Amazon,[2]
+          Google, Facebook, Microsoft and Twitter to permit the users to share
+          information about their accounts with third party applications or
+          websites. Generally, OAuth provides clients a "secure delegated
+          access" to server resources on behalf of a resource owner. It
+          specifies a process for resource owners to authorize third-party
+          access to their server resources without sharing their credentials.
+          Designed specifically to work with Hypertext Transfer Protocol (HTTP),
+          OAuth essentially allows access tokens to be issued to third-party
+          clients by an authorization server, with the approval of the resource
+          owner. The third party then uses the access token to access the
+          protected resources hosted by the resource server.[3] OAuth is a
+          service that is complementary to and distinct from OpenID. OAuth is
+          unrelated to OATH, which is a reference architecture for
+          authentication, not a standard for authorization. However, OAuth is
+          directly related to OpenID Connect (OIDC), since OIDC is an
+          authentication layer built on top of OAuth 2.0. OAuth is also
+          unrelated to XACML, which is an authorization policy standard. OAuth
+          can be used in conjunction with XACML, where OAuth is used for
+          ownership consent and access delegation whereas XACML is used to
+          define the authorization policies (e.g., managers can view documents
+          in their region).
+        </p>
+        <a href={this.state.redirect_uri} className="btn btn-block btn-primary">
+          <span className="fa fa-google"></span> Login
+        </a>
 
-                <Link to='/auth'>Auth</Link>
-                <button className="btn btn-danger" onClick={this.revokeToken}>Logout</button>
-                
-            </div>
-        )
-    }
+        {/* <Link to='/auth'>Auth</Link> */}
+        {/* <button className="btn btn-danger" onClick={this.revokeToken}> 
+          Logout
+        </button> */}
+      </div>
+    );
+  }
 }
 
-export default Homepage
+export default Homepage;
