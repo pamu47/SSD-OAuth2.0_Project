b34591d117aa00036c2163022fb9b0cbc08e7f19
pamu47 <pamudithamaduranga@gmail.com>
2020-10-06 21:08:00 +0530

    New contents on homepage and updates

--- client/src/components/Homepage.jsx
+++ client/src/components/Homepage.jsx
@@ -1,6 +1,5 @@
 import React, { Component } from "react";
 import Axios from "axios";
-import { Link } from "react-router-dom";
 
 class Homepage extends Component {
   constructor(props) {
@@ -32,32 +31,9 @@ class Homepage extends Component {
         <h2 style={{ paddingTop: 40 }}>oAuth 2.0</h2>
         
         <p className="text-justify">
-          OAuth is an open standard for access delegation, commonly used as a
-          way for Internet users to grant websites or applications access to
-          their information on other websites but without giving them the
-          passwords.[1] This mechanism is used by companies such as Amazon,[2]
-          Google, Facebook, Microsoft and Twitter to permit the users to share
-          information about their accounts with third party applications or
-          websites. Generally, OAuth provides clients a "secure delegated
-          access" to server resources on behalf of a resource owner. It
-          specifies a process for resource owners to authorize third-party
-          access to their server resources without sharing their credentials.
-          Designed specifically to work with Hypertext Transfer Protocol (HTTP),
-          OAuth essentially allows access tokens to be issued to third-party
-          clients by an authorization server, with the approval of the resource
-          owner. The third party then uses the access token to access the
-          protected resources hosted by the resource server.[3] OAuth is a
-          service that is complementary to and distinct from OpenID. OAuth is
-          unrelated to OATH, which is a reference architecture for
-          authentication, not a standard for authorization. However, OAuth is
-          directly related to OpenID Connect (OIDC), since OIDC is an
-          authentication layer built on top of OAuth 2.0. OAuth is also
-          unrelated to XACML, which is an authorization policy standard. OAuth
-          can be used in conjunction with XACML, where OAuth is used for
-          ownership consent and access delegation whereas XACML is used to
-          define the authorization policies (e.g., managers can view documents
-          in their region).
+        The application is capable of using google authentication and google drive functionalities to perform simple file uploading tasks. The application is implemented using Nodejs and Reactjs and the googleapis npm package is used in the backend Nodejs server to access google services APIs. The Google APIs are accessed after performing a successful oAuth 2.0 authorization and retrieving the tokens. 
         </p>
+        <br/>
         <a href={this.state.redirect_uri} className="btn btn-block btn-primary">
           <span className="fa fa-google"></span> Login
         </a>
